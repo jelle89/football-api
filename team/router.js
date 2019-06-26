92b6e2715f9d7bca40f09d5d54500eb6905742f7
Jelle Monen <jelle.monen@hotmail.com>
2019-06-26 16:12:25 +0200

    lol

--- team/router.js
+++ team/router.js
@@ -11,6 +11,13 @@ router.get(
     .catch(error => next(error))
 )
 
+router.get('/team/:id', function (request, response, next) {
+    const id = request.params.id
+    Team.findByPk().then(teams => {
+      response.json({ message: `Read team ${id}` })
+    })
+  })
+
 router.post(
     '/team',
     (request, response, next) => Team
@@ -18,5 +25,15 @@ router.post(
     .then(teams => response.json(teams))
     .catch(error => next(error))
 )
+
+router.put('/team/:id', function (request, response) {
+    const id = request.params.id
+    response.json({ message: `Update team ${id}` })
+  })
+
+  Team.update({
+      name: 'lol'},
+      { where: {}}).then(team => console.log(`The team with ID ${team.id} is now updated`))
+  
 
 module.exports = router
